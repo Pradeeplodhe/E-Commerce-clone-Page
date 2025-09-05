55b1a405f70b842553e39e59b842ef31a36c4559
Pradeep lodhi <lodhipradeep948@gmail.com>
2025-09-05 13:41:41 +0530

    initial

---
+++ 1.js
@@ -0,0 +1,96 @@
+ 
+ // get data
+ 
+ 
+//   let bag = [];
+//  let cart=JSON.parse(localStorage.getItem("cartarr")) ||[]
+ 
+ 
+//  fetch("https://fakestoreapi.com/products")
+// .then((res)=> res.json()).then((data)=> {
+//         console.log(data) 
+//         displayCard(data)
+        
+//      })
+//      .catch((err) => console.log(err.message))
+
+// //   let div=document.querySelector("#cont")
+
+//   //make a card
+//   function displayCard(arr){
+//      document.querySelector(".cont").innerText=""
+//     arr.forEach((el) => {
+        
+//      const box = document.createElement("div")
+
+//      const image = document.createElement("img");
+//      image.setAttribute("src",el.image)
+       
+           
+//      const title = document.createElement("h3");
+//      title.innerText=`Title: ${el.title}`;
+
+
+//      const descreption = document.createElement("p");
+//    descreption.innerText=`Title:  ${el.description}`;
+
+//  const category = document.createElement("p");
+// category.innerText=`Title:  ${el.category}`;
+
+
+// const price = document.createElement("button");
+//     price.innerText=`Title:  ${el.price}`;
+
+
+//     const rating = document.createElement("button");
+//      rating.innerText=`Title:  ${el.rating.rate}`;
+
+
+
+//   const hrr = document.createElement("hr")
+
+
+//   const Add=document.createElement("button")
+//           Add.addEventListener("click",function (){
+//        console.log(el)
+//          dataArr.push(el)
+//          localStorage.setItem("ecom",JSON.stringify(dataArr))
+//            console.log(dataArr,typeof(dataArr))
+        
+//         })
+//  Add.innerText=`Add to cart`;
+
+
+
+//  box.append(image,title,descreption,category,price,rating , hrr,Add)
+
+
+
+
+//     document.querySelector(".cont").append(box)
+
+
+
+
+
+    
+    
+//     });
+        
+//          //  div.Style.backgroundColors="red"
+         
+//   }
+
+//   function sch() {
+//      const query = document.querySelector("#search").value;
+//      console.log(query);
+//       let searchArr =bag.filter((el) =>{
+//         return el.title.toLowerCase().includes(query.toLowerCase())
+//       })
+//     displayCard(searchArr)
+//   }
+
+
+//   // function sort(){
+//   //   cons
+//   // }
